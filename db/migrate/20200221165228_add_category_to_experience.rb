6c99d8ab8810c376e6878a6e512ffecb83da2a7f
Basile Quinchon <basile.quinchon@gmail.com>
2020-03-28 16:44:12 +0100

    delete migrations

--- db/migrate/20200221165228_add_category_to_experience.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoryToExperience < ActiveRecord::Migration[5.2]
-  def change
-    add_column :experiences, :category, :string
-  end
-end
--- db/migrate/20200222150726_add_current_to_experience.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrentToExperience < ActiveRecord::Migration[5.2]
-  def change
-    add_column :experiences, :current, :boolean
-  end
-end
